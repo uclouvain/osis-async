08a66035e79f6983de4cbf10a120cfab7f00dc0b
jmr <jmr@makina-corpus.com>
2021-07-07 16:00:24 +0200

    serializer datetime with correct format

--- api/serializers.py
+++ api/serializers.py
@@ -4,6 +4,10 @@ from osis_async.models import AsyncTask
 
 
 class AsyncTaskSerializer(serializers.ModelSerializer):
+    created_at = serializers.DateTimeField(format='%d/%m/%Y %H:%M')
+    started_at = serializers.DateTimeField(format='%d/%m/%Y %H:%M')
+    completed_at = serializers.DateTimeField(format='%d/%m/%Y %H:%M')
+
     class Meta:
         model = AsyncTask
         fields = [
