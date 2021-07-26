77bc17e76cdb8cc17c407090a75c19c4105f5b25
jmr <jmr@makina-corpus.com>
2021-07-26 17:12:33 +0200

    AsyncTask factory

---
+++ tests/factory.py
@@ -0,0 +1,13 @@
+import factory
+
+from base.tests.factories.person import PersonFactory
+from osis_async.models import AsyncTask
+
+
+class AsyncTaskFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = AsyncTask
+
+    person = factory.SubFactory(PersonFactory)
+    name = factory.Faker("name")
+    description = factory.Faker("name")
