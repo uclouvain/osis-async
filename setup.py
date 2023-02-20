7393f9d09aba629fa15c1cd8dcd70e494cb2ccf3
unknown <alessandro.brugnetti@uclouvain.be>
2023-02-20 10:02:48 +0100

    Increase setup package version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='OSIS Async',
-    version='0.1',
+    version='0.2',
     description='Asynchronous tasks API and UI',
     url='http://github.com/uclouvain/osis-async',
     author='Université catholique de Louvain',
